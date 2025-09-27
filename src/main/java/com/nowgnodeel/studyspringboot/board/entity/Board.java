2dfa577f4044a5550fb556de541fbd31bee24ead
nowgnodeel <168517856+nowgnodeel123@users.noreply.github.com>
2025-09-27 17:50:22 +0900

    add: create Board class

---
+++ src/main/java/com/nowgnodeel/studyspringboot/board/entity/Board.java
@@ -0,0 +1,26 @@
+package com.nowgnodeel.studyspringboot.board.entity;
+
+import com.nowgnodeel.studyspringboot.global.entity.Timestamped;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Board extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+}
