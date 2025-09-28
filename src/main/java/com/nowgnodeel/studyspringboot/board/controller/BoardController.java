b1b40889d5816123da9755bdd96d294c346759e2
nowgnodeel <168517856+nowgnodeel123@users.noreply.github.com>
2025-09-28 22:10:25 +0900

    feat: add function on createBoard

--- src/main/java/com/nowgnodeel/studyspringboot/board/controller/BoardController.java
+++ src/main/java/com/nowgnodeel/studyspringboot/board/controller/BoardController.java
@@ -1,11 +1,30 @@
 package com.nowgnodeel.studyspringboot.board.controller;
 
+import com.nowgnodeel.studyspringboot.board.dto.CreateBoardRequestDto;
+import com.nowgnodeel.studyspringboot.board.dto.CreateBoardResponseDto;
+import com.nowgnodeel.studyspringboot.board.entity.Board;
+import com.nowgnodeel.studyspringboot.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/boards")
 public class BoardController {
+
+    private final BoardService boardService;
+
+    @PostMapping("/write")
+    public ResponseEntity<CreateBoardResponseDto> createBoard(@RequestBody CreateBoardRequestDto requestDto) {
+        Board board = boardService.createBoard(requestDto);
+        CreateBoardResponseDto responseDto = CreateBoardResponseDto.toDto(board);
+        URI location = URI.create("/boards/" + board.getId());
+        return ResponseEntity.created(location).body(responseDto);
+    }
 }
--- src/main/java/com/nowgnodeel/studyspringboot/board/service/BoardService.java
+++ src/main/java/com/nowgnodeel/studyspringboot/board/service/BoardService.java
@@ -1,9 +1,21 @@
 package com.nowgnodeel.studyspringboot.board.service;
 
+import com.nowgnodeel.studyspringboot.board.dto.CreateBoardRequestDto;
+import com.nowgnodeel.studyspringboot.board.entity.Board;
+import com.nowgnodeel.studyspringboot.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    @Transactional
+    public Board createBoard(CreateBoardRequestDto requestDto) {
+        Board board = Board.toEntity(requestDto);
+        return boardRepository.save(board);
+    }
 }
