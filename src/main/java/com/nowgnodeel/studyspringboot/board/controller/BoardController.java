c73f93afdecace5510fd601458dd79c0fabfbde0
nowgnodeel <168517856+nowgnodeel123@users.noreply.github.com>
2025-09-28 20:15:59 +0900

    add: create Board Controller

---
+++ src/main/java/com/nowgnodeel/studyspringboot/board/controller/BoardController.java
@@ -0,0 +1,11 @@
+package com.nowgnodeel.studyspringboot.board.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/boards")
+public class BoardController {
+}
