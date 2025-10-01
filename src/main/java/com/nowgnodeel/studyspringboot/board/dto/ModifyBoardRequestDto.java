41d6973e9b41f61f440bf0a1605e08b08e393265
nowgnodeel <168517856+nowgnodeel123@users.noreply.github.com>
2025-10-02 00:34:33 +0900

    modify: add Category

--- src/main/java/com/nowgnodeel/studyspringboot/board/dto/ModifyBoardRequestDto.java
+++ src/main/java/com/nowgnodeel/studyspringboot/board/dto/ModifyBoardRequestDto.java
@@ -1,7 +1,10 @@
 package com.nowgnodeel.studyspringboot.board.dto;
 
+import com.nowgnodeel.studyspringboot.board.entity.Category;
+
 public record ModifyBoardRequestDto(
         String title,
-        String content
+        String content,
+        Category category
 ) {
 }
