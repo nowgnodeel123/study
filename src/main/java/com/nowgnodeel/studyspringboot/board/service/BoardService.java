cdc700b87fb14d3bd6407426deb37d7ff7e2b933
nowgnodeel <168517856+nowgnodeel123@users.noreply.github.com>
2025-10-13 22:34:07 +0900

    feat: add function on read all board

--- src/main/java/com/nowgnodeel/studyspringboot/board/service/BoardService.java
+++ src/main/java/com/nowgnodeel/studyspringboot/board/service/BoardService.java
@@ -5,6 +5,8 @@ import com.nowgnodeel.studyspringboot.board.dto.ModifyBoardRequestDto;
 import com.nowgnodeel.studyspringboot.board.entity.Board;
 import com.nowgnodeel.studyspringboot.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -36,5 +38,13 @@ public class BoardService {
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Board not found"));
         board.patch(requestDto);
         return boardRepository.save(board);
+    }
+
+    @Transactional
+    public Page<Board> getBoardList(Pageable pageable) {
+        if (boardRepository.findAll(pageable).isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Board not found");
+        }
+        return boardRepository.findAll(pageable);
     }
 }
