8af69efbdb584fffffa6cc8dc9d9473451332dd2
nowgnodeel <168517856+nowgnodeel123@users.noreply.github.com>
2025-10-13 22:34:02 +0900

    feat: add function on read all board

--- src/main/java/com/nowgnodeel/studyspringboot/board/controller/BoardController.java
+++ src/main/java/com/nowgnodeel/studyspringboot/board/controller/BoardController.java
@@ -1,12 +1,12 @@
 package com.nowgnodeel.studyspringboot.board.controller;
 
-import com.nowgnodeel.studyspringboot.board.dto.CreateBoardRequestDto;
-import com.nowgnodeel.studyspringboot.board.dto.CreateBoardResponseDto;
-import com.nowgnodeel.studyspringboot.board.dto.ModifyBoardRequestDto;
-import com.nowgnodeel.studyspringboot.board.dto.ModifyBoardResponseDto;
+import com.nowgnodeel.studyspringboot.board.dto.*;
 import com.nowgnodeel.studyspringboot.board.entity.Board;
 import com.nowgnodeel.studyspringboot.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -37,6 +37,13 @@ public class BoardController {
     public ResponseEntity<ModifyBoardResponseDto> modifyBoard(@PathVariable Long boardId, @RequestBody ModifyBoardRequestDto requestDto) {
         Board board = boardService.modifyBoard(boardId, requestDto);
         ModifyBoardResponseDto responseDto = ModifyBoardResponseDto.toDto(board);
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<GetBoardListResponseDto>> getBoardList(@PageableDefault Pageable pageable) {
+        Page<Board> boardList = boardService.getBoardList(pageable);
+        Page<GetBoardListResponseDto> responseDto = GetBoardListResponseDto.toDto(boardList);
         return ResponseEntity.ok(responseDto);
     }
 }
