b85c5cc2986c56ab7f3638126dc52734630ac5ad
nowgnodeel <168517856+nowgnodeel123@users.noreply.github.com>
2025-10-15 23:01:35 +0900

    add: create CommentController

---
+++ src/main/java/com/nowgnodeel/studyspringboot/comment/controller/CommentController.java
@@ -0,0 +1,14 @@
+package com.nowgnodeel.studyspringboot.comment.controller;
+
+import com.nowgnodeel.studyspringboot.comment.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/comments")
+public class CommentController {
+
+    private final CommentService commentService;
+}
