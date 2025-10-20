ebb6a22c88aea46b8999c95fca00dbb86ef5c0d7
nowgnodeel <168517856+nowgnodeel123@users.noreply.github.com>
2025-10-20 23:13:09 +0900

    modify: adding createComment function, but not yet

--- src/main/java/com/nowgnodeel/studyspringboot/comment/controller/CommentController.java
+++ src/main/java/com/nowgnodeel/studyspringboot/comment/controller/CommentController.java
@@ -1,9 +1,14 @@
 package com.nowgnodeel.studyspringboot.comment.controller;
 
+import com.nowgnodeel.studyspringboot.comment.dto.CreateCommentRequestDto;
+import com.nowgnodeel.studyspringboot.comment.dto.CreateCommentResponseDto;
+import com.nowgnodeel.studyspringboot.comment.entity.Comment;
 import com.nowgnodeel.studyspringboot.comment.service.CommentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,4 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class CommentController {
 
     private final CommentService commentService;
+
+    @PostMapping("/{boardId}")
+    public ResponseEntity<CreateCommentResponseDto> createComment(@PathVariable("boardId") Long boardId, @RequestBody CreateCommentRequestDto requestDto) {
+        Comment comment = commentService.createComment(boardId, requestDto);
+        CreateCommentResponseDto responseDto = CreateCommentResponseDto.toDto(comment);
+        URI location = URI.create("/comments/" + comment.getId());
+        return ResponseEntity.created(location).body(responseDto);
+    }
 }
