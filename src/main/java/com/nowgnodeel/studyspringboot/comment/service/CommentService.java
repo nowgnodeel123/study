48a9fc0b2b1d0e883b81acb55611c1b8abe35e84
nowgnodeel <168517856+nowgnodeel123@users.noreply.github.com>
2025-10-20 23:13:11 +0900

    modify: adding createComment function, but not yet

--- src/main/java/com/nowgnodeel/studyspringboot/comment/service/CommentService.java
+++ src/main/java/com/nowgnodeel/studyspringboot/comment/service/CommentService.java
@@ -1,12 +1,28 @@
 package com.nowgnodeel.studyspringboot.comment.service;
 
+import com.nowgnodeel.studyspringboot.board.repository.BoardRepository;
+import com.nowgnodeel.studyspringboot.comment.dto.CreateCommentRequestDto;
+import com.nowgnodeel.studyspringboot.comment.entity.Comment;
 import com.nowgnodeel.studyspringboot.comment.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
 
     private final CommentRepository commentRepository;
+    private final BoardRepository boardRepository;
+
+    @Transactional
+    public Comment createComment(Long boardId, CreateCommentRequestDto requestDto) {
+        if (!boardRepository.existsById(boardId)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Board not found");
+        }
+        Comment comment = Comment.toEntity(requestDto);
+        return commentRepository.save(comment);
+    }
 }
